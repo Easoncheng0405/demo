da0272777bfa51ee90dae4fc7446ebbff5981ae8
xudong21150713 <m15024730334_1@163.com>
2019-05-04 21:26:00 +0800

    new

--- src/main/java/com/jlu/xudong/demo/DealController.java
+++ src/main/java/com/jlu/xudong/demo/DealController.java
@@ -29,8 +29,8 @@ public class DealController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/dealIncome")
-    public Response<List<Deal>> income(int id) { // 用户id
+    @GetMapping("/dealShowAll")
+    public Response<List<Deal>> showAll(int id) { // 用户id
         Response<List<Deal>> response = new Response<>();
         User user = new User();
         user = userRepository.findById(id).get();
@@ -43,14 +43,14 @@ public class DealController {
         return response;
     }
 
-    @GetMapping("/dealIncomeSelect")
-    public Response<Deal> incomeSelect(int id) { // 收入记录id
+    @GetMapping("/dealshowSelect")
+    public Response<Deal> showSelect(int id) { // 收入记录id
         Response<Deal> response = new Response<>();
         response.body = dealRepository.findById(id).get();
         return response;
     }
 
-    @GetMapping("/dealSaveIncome")
+    @GetMapping("/dealSave")
     public Response<Deal> save(String type, double cash, int id, boolean mark) {
         Response<Deal> response = new Response<>();
         User user = new User();
@@ -66,14 +66,14 @@ public class DealController {
         response.body.type = type;
         response.body.date = new Date();
         response.body.user = user;
-        response.body.mark = true;
+        response.body.mark = mark;
 
         response.body = dealRepository.save(response.body);
         return response;
     }
 
-    @GetMapping("/dealDeleteAllIncome")
-    public Response<Void> deleteAllIncome(int id) {
+    @GetMapping("/dealDeleteAll")
+    public Response<Void> deleteAll(int id) {
         Response<Void> response = new Response<>();
         User user = userRepository.findById(id).get();
         List<Deal> list = dealRepository.findByUser(user);
@@ -83,114 +83,20 @@ public class DealController {
         return response;
     }
 
-    @GetMapping("/dealDeleteIncome")
-    public Response<Void> deleteIncome(int id) {
+    @GetMapping("/dealDeleteselect")
+    public Response<Void> deleteSelect(int id) {
         Response<Void> response = new Response<>();
         dealRepository.deleteById(id);
         return response;
     }
 
-    @GetMapping("/dealUpdataIncome")
-    public Response<Deal> updataIncome(int id, String type, double cash) {
+    @GetMapping("/dealUpdata")
+    public Response<Deal> updata(int id, String type, double cash) {
         Response<Deal> response = new Response<>();
         response.body = dealRepository.findById(id).get();
         response.body.cash = cash;
         response.body.type = type;
         dealRepository.save(response.body);
-        return response;
-    }
-
-    @GetMapping("/dealOutcome")
-    public Response<List<Deal>> outcome(int id) {
-        Response<List<Deal>> response = new Response<>();
-        User user = userRepository.findById(id).get();
-
-        if (user == null) {
-            response.body = new ArrayList<>(0);
-            return response;
-        }
-        response.body = dealRepository.findByUser(user);
-        return response;
-    }
-
-    @GetMapping("/dealOutcomeSelect")
-    public Response<Deal> outcomeSelect(int id) {
-        Response<Deal> response = new Response<>();
-        response.body = dealRepository.findById(id).get();
-        return response;
-    }
-
-    @GetMapping("/dealSaveOutCome")
-    public Response<Deal> save(int id, String type, double cash, boolean mark) {
-        Response<Deal> response = new Response<>();
-        User user = userRepository.findById(id).get();
-
-        if (user == null)
-            return null;
-
-        response.body = new Deal();
-        response.body.cash = cash;
-        response.body.type = type;
-        response.body.date = new Date();
-        response.body.user = user;
-        response.body.mark = false;
-        response.body = dealRepository.save(response.body);
-
-        return response;
-    }
-
-    @GetMapping("/dealSelectAllOutcome")
-    public Response<Void> delectAllOutcome(int id) {
-        Response<Void> response = new Response<>();
-        User user = userRepository.findById(id).get();
-        List<Deal> list = dealRepository.findByUser(user);
-        for (Deal i : list) {
-            dealRepository.delete(i);
-        }
-        return response;
-    }
-
-    @GetMapping("/dealSeleteOutcome")
-    public Response<Void> deleteOutcome(int id) {
-        Response<Void> response = new Response<>();
-        dealRepository.deleteById(id);
-        return response;
-    }
-
-    @GetMapping("/dealUpdataOutcomeType")
-    public Response<Deal> updataOutcometype(int id, String type) {
-        Response<Deal> response = new Response<>();
-        response.body = dealRepository.findById(id).get();
-        response.body.type = type;
-        if (dealRepository.save(response.body) != null) {
-            response.status = 200;
-            response.msg = "success";
-        }
-        return response;
-    }
-
-    @GetMapping("/dealUpdataOutcomeCash")
-    public Response<Deal> updataOutcomecash(int id, double cash) {
-        Response<Deal> response = new Response<>();
-        response.body = dealRepository.findById(id).get();
-        response.body.cash = cash;
-        if (dealRepository.save(response.body) != null) {
-            response.status = 200;
-            response.msg = "success";
-        }
-        return response;
-    }
-
-    @GetMapping("/dealUpdataOutcome")
-    public Response<Deal> updataOutcome(int id, String type, double cash) {
-        Response<Deal> response = new Response<>();
-        response.body = dealRepository.findById(id).get();
-        response.body.cash = cash;
-        response.body.type = type;
-        if (dealRepository.save(response.body) != null) {
-            response.status = 200;
-            response.msg = "success";
-        }
         return response;
     }
 }
